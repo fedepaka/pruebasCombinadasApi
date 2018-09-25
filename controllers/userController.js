6c27c6d4c49bec87d45cb783af54028553cbad22
federico ramirez <framirez@folderit.net>
2018-09-25 16:27:07 -0300

    - encriptación del password

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,46 +4,59 @@ var util = require('./utilController');
 
 const {check, validationResult} = require('express-validator/check');
 
-// Display list of all user.
-exports.user_list = function (req, res) {
-    res.send('NOT IMPLEMENTED: ser list');
-};
-
-// Handle User by Id
-exports.user_byId = function (req, res) {
-    res.send('NOT IMPLEMENTED: User detail: ' + req.params.id);
-};
-
 // Handle User create.
 exports.user_create = function (req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({errors: errors.array()});
     }
 
     if (req && req.body) {
-        var encryptPass = util.encrypt(req.body.password);
-        console.log('pepe - '+encryptPass)
-        const user = {
-            username: req.body.username,
-            password: encryptPass,//req.body.password,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            dateBirth: req.body.dateBirth,
-            createdAt: new Date()
-        };
-
-        console.log(user);
-
-        // normal processing here
-        model.User.create(user).then(function (created) {
-            res.send(created);
+        getHashPassword(req.body.password).then(pass => {
+            const user = {
+                username: req.body.username,
+                password: pass,
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                dateBirth: req.body.dateBirth,
+                createdAt: new Date()
+            };
+            //console.log(user);
+            // normal processing here
+            model.User.create(user).then(function (created) {
+                res.send(created);
+            });
         });
-
     } else {
         return res.json({result: "error", message: "error"});
     }
 };
+
+exports.validateUser = [
+    check('username').isEmail().withMessage('el username debe ser un email.'),
+    check('password').isNumeric().isLength({min: 8}).withMessage('el password debe ser alfanumérico con mínimo de 8 caracteres.'),
+    check('firstname').isLength({min: 1}).withMessage('el nombre no puede ser vacío'),
+    check('lastname').isLength({min: 1}).withMessage('el apellido no puee ser vacío'),
+    check('username').custom((value, req) => {
+        return getUserByUsername(req, value).then(user => {
+            if (user) {
+                return Promise.reject('El usuario ya existe.');
+            }
+        });
+    }).withMessage('El usuario ya existe.')
+];
+
+function getUserByUsername(req, username) {
+    return model.User.getUserByUsername(username);
+}
+
+function getHashPassword(username) {
+    return util.encrypt(username).then(function (hash) {
+        return hash;
+    }).catch(err => {
+        throw (err);
+    })
+}
 
 // Handle User Update
 exports.user_update = function (req, res) {
@@ -55,25 +68,12 @@ exports.user_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: User delete: ' + req.params.id);
 };
 
-// exports.saludo = saludo;
-// exports.saludoPost = saludoPost;
+// Display list of all user.
+exports.user_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: ser list');
+};
 
-exports.validateUser = [
-    check('username').isEmail().withMessage('el username debe ser un email.'),
-    check('password').isNumeric().isLength({min: 8}).withMessage('el password debe ser alfanumérico con mínimo de 8 caracteres.'),
-    check('firstname').isLength({min: 1}).withMessage('el nombre no puede ser vacío'),
-    check('lastname').isLength({min: 1}).withMessage('el apellido no puee ser vacío'),
-
-    
-    check('username').custom((value, req ) => {
-        return getUserByUsername(req, value).then(user => {
-            if(user){
-                return Promise.reject('El usuario ya existe.');
-            }
-        });
-    }).withMessage('El usuario ya existe.')
-];
-
-function getUserByUsername(req, username) {
-    return model.User.getUserByUsername(username);
-}
+// Handle User by Id
+exports.user_byId = function (req, res) {
+    res.send('NOT IMPLEMENTED: User detail: ' + req.params.id);
+};
--- controllers/utilController.js
+++ controllers/utilController.js
@@ -3,29 +3,15 @@ var salt = bcrypt.genSaltSync();
 
 
 exports.encrypt = function (text){
-    // var cipher = crypto.createCipher(algorithm,password)
-    // var crypted = cipher.update(text,'utf8','hex')
-    // crypted += cipher.final('hex');
-    // return crypted;
-
-    var hash = bcrypt.hash(text, salt, function(err, hash) {
-        //console.log("paka" + hash);
-        // Store hash in database
-        return hash;
+    return bcrypt.hash(text, salt).catch(err => {
+        throw (err);
     });
-
-    return hash;
 }
 
 exports.decrypt = function (passwordToCompare, paswordEncripted){
-    // var decipher = crypto.createDecipher(algorithm,password)
-    // var dec = decipher.update(text,'hex','utf8')
-    // dec += decipher.final('utf8');
-    // return dec;
-
     bcrypt.compare(passwordToCompare, paswordEncripted, function(err, res) {
-        // res == true
-        if(err) next();
         return res;
+    }).catch(err => {
+        throw (err);
     });
 }
